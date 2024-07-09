326e49907f5e53373620148cbbf641ae02d20c3a
Agustin Castillo <agustingacastillo@gmail.com>
2024-07-09 19:55:28 -0300

    Removing filelist type from Node

--- src/utils/zodSchemas.tsx
+++ src/utils/zodSchemas.tsx
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const sportSchema = z.object({
     name: z.string().min(4, { message: 'Nome deve ter mais de 6 caracteres'}),
     playersPerTeam: z.preprocess((ppt) => parseInt(z.string().parse(ppt),10),z.number().min(1, { message: 'Número de jogadores por equipe deve ser maior do que 0'}).max(53, {message: "Número de jogadores por equipe deve ser menor que 53"})),
-    rules: z.instanceof(FileList)
+    rules: z.any()
 });
