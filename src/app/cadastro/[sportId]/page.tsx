546a45dbe210274054212fff209bc74c4513ad21
Agustin Castillo <agustingacastillo@gmail.com>
2024-07-09 20:05:31 -0300

    Removing filelist error in frontend for single sport

--- src/app/cadastro/[sportId]/page.tsx
+++ src/app/cadastro/[sportId]/page.tsx
@@ -69,7 +69,6 @@ const SingleSportPage: React.FC = () => {
                         {...register('rules')}
                     />
                     <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-300 dark:peer-focus:text-primary">Livro de Regras</label>
-                    {errors.rules && <span className='text-mikasa-red text-sm text-center pt-2 mx-auto'>{errors.rules.message}</span>}
                 </div>
                 {editing && <button disabled={isSubmitting} type="submit" className="inline-block w-full rounded bg-blue-400 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2">
                     {isSubmitting ? 'Salvando...' : 'Salvar'}
